0a8ff25f6961a35891f3022e0a45db65f70b48b9
Vamsee NY <vamsee.ny1@gmail.com>
2024-02-19 20:53:15 +0530

    create interface script

---
+++ interface.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Demo-Interface v0.0",
+    layout='wide'
+)
+
+st.write("Demo Interface v0.0")
+
+#def check_desc():
+    #parse input to llm and return feedback
+
+create,results=st.tabs(['Create post','View results'])
+
+with create:
+    job_title=st.text_input("Enter job title")
+    job_description=st.text_input("Enter job purpose")
+    ed_qual=st.text_input("Enter educational qualifications")
+    skill_qual=st.text_input("Enter required skills")
+    loc=st.text_input("Enter job location")
+    comp=st.text_input("Enter salary")
+
+
+    review=st.button("Deploty")
+
+
